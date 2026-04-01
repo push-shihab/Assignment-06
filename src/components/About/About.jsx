ec17bc12c2f211bb018ea1b9f7626f8a97fc6e6e
push-shihab <shihabfamily539@gmail.com>
2026-04-01 16:39:34 +0600

    Design of About section done

---
+++ src/components/About/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white px-50 py-15 flex justify-center items-center gap-20">
+      <div>
+        <h2 className="font-extrabold text-6xl">50K+</h2>
+        <p className="font-medium text-2xl">Active Users</p>
+      </div>
+      <div className="divider divider-horizontal"></div>
+      <div>
+        <h2 className="font-extrabold text-6xl">200+</h2>
+        <p className="font-medium text-2xl">Premium Tools</p>
+      </div>
+      <div className="divider divider-horizontal"></div>
+      <div>
+        <h2 className="font-extrabold text-6xl">4.9</h2>
+        <p className="font-medium text-2xl">Rating</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Hero from "../Hero/Hero";
+import About from "../About/About";
 
 const Header = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
+      <About></About>
     </div>
   );
 };
